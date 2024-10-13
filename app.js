37325f0f6d082ba0b14ec324812dfa2ed0d5e5d4
EnzoDemitrius10 <enzodemitrius10@gmail.com>
2024-10-12 22:29:19 -0300

    final cors test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.CLIENT_URL | 'http://localhost:3000',
+  origin: 'https://memory-box-rho.vercel.app',
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'PUT'],
 }));
 
